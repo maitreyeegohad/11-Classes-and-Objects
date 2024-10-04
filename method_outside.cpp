cbd8385365783450f34e4a5e9b3d5182ed7d374d
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-10-04 16:44:54 +0530

    Create method_outside.cpp

---
+++ method_outside.cpp
@@ -0,0 +1,25 @@
+//Defining method outside class.
+#include<iostream>
+using namespace std;
+
+class student
+{
+    public:
+    void myMethod();
+};
+
+void student::myMethod()
+{
+    cout<<"Studying in 2nd year.";
+}
+
+int main()
+{
+    student s1;
+    s1.myMethod();
+}
+
+/*
+OUTPUT: 
+Studying in 2nd year.
+*/
